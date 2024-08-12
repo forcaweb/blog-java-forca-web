f29e091f9b734bae98fe174bb6904928b56affdd
forcaweb <forcawebsuporte@gmail.com>
2024-08-12 15:23:04 -0300

    Add Cors

--- src/main/java/net/forcaweb/resources/PostingResource.java
+++ src/main/java/net/forcaweb/resources/PostingResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import net.forcaweb.entities.Posting;
 import net.forcaweb.services.PostingService;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:5500"})
 @RequestMapping(value = "/postings/")
 public class PostingResource {
 
